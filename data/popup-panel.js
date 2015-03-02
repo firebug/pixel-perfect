ecc1ff9aae3f350edf233b135084dc9749c721b1
Jan Odvarko <odvarko@gmail.com>
2015-03-02 17:20:50 +0100

    Update JS docs for popup panel

--- data/popup-panel.js
+++ data/popup-panel.js
@@ -13,7 +13,13 @@ const { LayerStore } = require("layer-store");
 const { TABLE, TR, TD, DIV, IMG, SPAN } = Reps.DOM;
 
 /**
- * TODO docs
+ * @react This template implements basic layout for the popup panel.
+ * There are two components displayed:
+ * 1. Layer list: list of all registered layers.
+ * 2. Layer form: a form displaying properties of the selected layer.
+ *
+ * If there are no layers, the popup panel displays default content
+ * with instructions and one button: 'Add Layer'.
  */
 var PopupPanel = React.createClass({
   getInitialState: function() {
@@ -35,6 +41,7 @@ var PopupPanel = React.createClass({
       });
     }
 
+    // Render list of layers and layer form components.
     return (
       TABLE({className: "", width: "100%"},
         TR({},
@@ -87,7 +94,9 @@ var PopupPanel = React.createClass({
 });
 
 /**
- * xxxHonza: TODO docs
+ * @react This template renders default content in case there are
+ * no layers registered. It displays basic instructions about how
+ * to begin with Pixel Perfect.
  */
 var DefaultContent = React.createFactory(React.createClass({
   render: function() {
