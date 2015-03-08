46b70b5b32e6e420413a0f74f6cfb9acd9a8b50b
Jan Odvarko <odvarko@gmail.com>
2015-03-08 11:10:00 +0100

    Test: invert layer (related to issue #34)

---
+++ test/test-layer-invert.js
@@ -0,0 +1,41 @@
+/* See license.txt for terms of usage */
+
+"use strict";
+
+const { click, addNewLayer, waitForEvents, removeLayer } = require("./common.js");
+const { closeTab } = require("sdk/tabs/utils");
+
+/**
+ * xxxHonza TODO docs
+ */
+exports["test Layer Invert"] = function(assert, done) {
+  addNewLayer().then(config => {
+    let popup = config.popup;
+    let layer = config.layer;
+
+    // Click on the 'Invert' checkbox.
+    click(popup, "#pixel-perfect-invert");
+
+    // Wait till the layer is modified on the backend.
+    waitForEvents(popup, ["layer-modified"]).then(result => {
+      console.log("!!! waitForEvents (invert) " + layer.id, result);
+
+      // Get layer info from the backend.
+      popup.front.getLayerInfo(layer.id).then(response => {
+        console.log("!!! invert", response)
+        assert.equal(response.content.invert, "true",
+          "The layer must be inverted");
+
+        // Clean up
+        removeLayer(popup, layer.id).then(() => {
+          config.popup.destroy().then(() => {
+            closeTab(config.tab);
+            done();
+          });
+        });
+      })
+    });
+  });
+};
+
+require("sdk/test").run(exports);
