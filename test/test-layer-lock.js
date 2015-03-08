4eae192da1e6140ba424152d51c4d919ed26fa8b
Jan Odvarko <odvarko@gmail.com>
2015-03-08 10:24:30 +0100

    Test: lock layer (related to issue #34)

---
+++ test/test-layer-lock.js
@@ -0,0 +1,36 @@
+/* See license.txt for terms of usage */
+
+"use strict";
+
+const { click, addNewLayer, waitForEvents } = require("./common.js");
+const { closeTab } = require("sdk/tabs/utils");
+
+/**
+ * xxxHonza TODO docs
+ */
+exports["test Layer Lock"] = function(assert, done) {
+  addNewLayer().then(config => {
+    let popup = config.popup;
+    let layer = config.layer;
+
+    // Click on the 'Lock' checkbox.
+    click(popup, "#pixel-perfect-lock");
+
+    // Wait till the layer is modified on the backend.
+    waitForEvents(popup, ["layer-modified"]).then(() => {
+      // Get layer info from the backend.
+      popup.front.getLayerInfo(layer.id).then(response => {
+        assert.equal(response.content.lock, "true",
+          "The layer must be locked");
+
+        // Clean up
+        config.popup.hide().then(() => {
+          closeTab(config.tab);
+          done();
+        });
+      })
+    });
+  });
+};
+
+require("sdk/test").run(exports);
